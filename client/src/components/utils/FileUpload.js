7717fbbba6ca4b7fb8348ee3a1044bb81793b584
psick22 <kgirok@gmail.com>
2021-01-06 02:08:20 +0900

    Add dropzone

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
+import { PlusOutlined } from '@ant-design/icons';
 
 function FileUpload() {
-  return <div>FileUpload</div>;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div
+              style={{
+                width: 300,
+                height: 240,
+                border: '1px solid lightgray',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+              <div>
+                <PlusOutlined
+                  style={{ fontSize: '2.5rem', color: 'darkgray' }}
+                />
+              </div>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+    </div>
+  );
 }
 
 export default FileUpload;
